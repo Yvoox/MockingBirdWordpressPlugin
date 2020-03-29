d223231ee85630aca2b7b93981bb4835aef84f93
Yvoox <alexandre-colicchio@hotmail.fr>
2020-03-29 11:26:27 +0200

    Update mocking-bird-plugin.php

--- mocking-bird-plugin/mocking-bird-plugin.php
+++ mocking-bird-plugin/mocking-bird-plugin.php
@@ -12,26 +12,25 @@
 
  function inject_mockingbird_bot ( $content ) {
      return $content .= '    <!-- Enjoy your MockingBird Bot ! -->
-     <style>
-       .chat-popup {display: none;position: fixed;bottom: 0;right: 15px;border: 3px solid #f1f1f1;z-index: 9;}
-       #chatPop{text-shadow: 0 0 3px white;;color:#367fa9;font-size: 70px;position: fixed;bottom: 65px;right: 30px;cursor:pointer;}
-     </style>
-     <a id="chatPop" onclick="openChat()"><i class="fa fa-comment" ></i></a>
-     <div class="chat-popup" id="chatBot">
-       <div style="display:inline-block;vertical-align:middle;">
-         <p style="position:absolute;right:0;"><button type="button" onclick="closeForm()">-</button></p>
-         <iframe id="chatIframe"
-           allow="microphone"
-           title="Your Chatbot"
-           height="500"
-           width="500"
-           src="http://localhost:8081/chat.html?token=MON_API_TOKEN">
-         </iframe>
-       </div>
-     </div>
-     <script>
-       function openChat() {document.getElementById("chatBot").style.display = "block";}
-       function closeForm() {document.getElementById("chatBot").style.display = "none";}
-     </script>
-     <!-- End MockingBird bot-->';
+    <style>
+      .chat-popup {display: none;position: fixed;bottom: 0;right: 15px;border: 3px solid #f1f1f1;z-index: 9;}
+      #chatPop{text-shadow: 0 0 3px white;;color:#367fa9;font-size: 70px;position: fixed;bottom: 65px;right: 30px;cursor:pointer;}
+    </style>
+    <a id="chatPop" onclick="openChat()"><i class="fa fa-comment"></i></a>
+    <div class="chat-popup" id="chatBot">
+      <div style="display:inline-block;vertical-align:middle;">
+        <p style="position:absolute;right:0;"><button type="button" onclick="closeForm()">-</button></p>
+        <iframe id="chatIframe"
+          allow="microphone"
+          title="Your Chatbot"
+          height="330"
+          src="https://www.mockingbird-bot.com/chat/chat.html?token=MON_API_TOKEN">
+        </iframe>
+      </div>
+    </div>
+    <script>
+      function openChat() {document.getElementById("chatBot").style.display = "block";}
+      function closeForm() {document.getElementById("chatBot").style.display = "none";}
+    </script>
+    <!-- End MockingBird bot-->';
  }
